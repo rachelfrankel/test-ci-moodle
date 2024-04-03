dd4c6ad3e8951d5d1e13a3ca05bd0a4a52877806
R. Frenkel <rachelf@skyvar.co.il>
2024-04-03 16:41:54 +0300

    succ

--- test/build_containers.sh
+++ test/build_containers.sh
@@ -3,5 +3,12 @@
 docker build -f Dockerfile.moodle -t moodle .
 docker build -f Dockerfile.mariadb -t mariadb .
 docker network create my-network300
-# docker run -d --env ${{secrets.MOODLE_OBJ}} --name moodle -p 80:8080 --network my-network300 moodle
-# docker run -d --name mariadb -p 3306:3306 --network my-network300 mariadb
+# docker run -d --env ${{secrets.MOODLE_OBJ}} --name moodle -p 80:8080 --net my-network300 moodle
+# docker run -d --name mariadb -p 3306:3306 --net my-network300 mariadb
+
+
+
+mariadb:
+# -e ALLOW_EMPTY_PASSWORD=yes \
+  -e MARIADB_USER=bn_moodle \
+  -e MARIADB_DATABASE=bitnami_moodle 
