4fe697b8d0fa3c4cd909d0f3add4519b3bc7c626
R. Frenkel <rachelf@skyvar.co.il>
2024-04-03 16:52:55 +0300

    succ

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,5 @@ sleep 10;
 docker cp test/feature/. moodle:/opt/bitnami/moodle/local/aiquestions/tests
 docker cp test/config.php moodle:/opt/bitnami/moodle/config.php
 docker exec moodle php opt/bitnami/moodle/admin/tool/phpunit/cli/init.php
-docker exec moodle bash -c "cd opt/bitnami/moodle && \
+docker exec moodle bash -c "cd bitnami/moodle && \
         vendor/bin/phpunit local/aiquestions/tests/convert_word_to_pdf_test.php"
